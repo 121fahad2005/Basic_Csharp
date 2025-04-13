3c325857668dec0d1b0d3b4fdf5a7f40a7b22e3a
121fahad2005 <121fahad2005@gmail.com>
2025-04-13 17:01:13 +0500

    Program.cs

--- Program.cs
+++ Program.cs
@@ -4,17 +4,17 @@ namespace Helloworld{
  class check{ 
          static void Main( String [] args)
          {
-          int a =50;                   // For interger numbers
-          float pi = 3.14f;            // For floating point numbers
-          double salary= 4500.76d;     // For decimal numbers
+          int n =5556;                   // For interger numbers
+           double sales_rev= 698222.555d;     // For decimal number
+          float e = 5689.22f;            // For floating point numbers
+         
+          
 
-          // Prints the values of the variables
+          System.Console.WriteLine("The value of n is"+n);
+          System.Console.WriteLine("The value of e is"+e);
+          System.Console.WriteLine("The value of sales_rev is"+sales_rev);
 
-          System.Console.WriteLine($"The value of a is {a}");
-          System.Console.WriteLine($"The value of pi is {pi}");
-          System.Console.WriteLine($"The value of salary is {salary}");
-
-          Console.ReadKey();
+        
          }
 
     }
